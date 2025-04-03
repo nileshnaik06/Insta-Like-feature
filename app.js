9c87690bf0fdbacfb2d031e65312188dd05d06a1
nileshnaik06 <you@example.com>
2025-04-04 00:16:41 +0530

    Fresh start

---
+++ app.js
@@ -0,0 +1,125 @@
+function anim() {
+let crsr = document.querySelector("#cursor");
+let main = document.querySelector("main");
+let nav = document.querySelector("nav");
+let h1 = document.querySelectorAll("h1");
+let lists = document.querySelectorAll("li");
+
+main.addEventListener("mousemove",function (dets) {
+    crsr.style.left = dets.x  + "px"
+    crsr.style.top = dets.y + "px"
+})
+
+lists.forEach((li)=>{
+    li.addEventListener("mouseenter",function () {
+        crsr.style.mixBlendMode = "color-burn";
+        crsr.style.transform = "scale(.8)"
+    })
+    li.addEventListener("mouseleave",function () {
+        crsr.style.mixBlendMode = "normal";
+        crsr.style.transform = "scale(1)"
+        
+    })
+})
+h1.forEach((header)=>{
+    header.addEventListener("mouseenter",function () {
+        crsr.style.mixBlendMode = "color-burn";
+    })
+    header.addEventListener("mouseleave",function () {
+        crsr.style.mixBlendMode = "normal";
+    })
+})
+
+
+
+window.addEventListener("wheel",function (det) {
+    if (det.deltaY > 0) {
+        nav.style.top = "-100px"; // Hide the nav (adjust value as needed)
+    } else {
+        nav.style.top = "0px"; // Show the nav
+    }
+    
+})
+
+}
+anim()
+
+function like () {
+let mainImages = document.querySelectorAll(".mainImg")
+let hearts = document.querySelectorAll(".heart")
+let likes = document.querySelectorAll(".hLike")
+
+mainImages.forEach((mImg, index) => {
+    mImg.addEventListener("dblclick", function (e) {
+        let heart = hearts[index]; // Get the corresponding heart element
+        let like = likes[index]; // Get the corresponding heart element
+        like.style.color = "#fa3636"
+        heart.style.opacity = "1";
+        heart.style.color = "#fa3636";
+        heart.style.transform = "scale(1.1)"; // Reset size
+        heart.style.transition = "opacity 0.5s ease-in, transform 0.3s ease-in-out";
+
+        // Hide the heart after 1 second
+        setTimeout(() => {
+        heart.style.opacity = "0";
+        }, 1000);
+    });
+});
+likes.forEach((like, index) => {
+    like.addEventListener("click", function (e) {
+        let heart = hearts[index]; // Get the corresponding heart element
+        like.style.color = "#fa3636"
+        heart.style.opacity = "1";
+        heart.style.color = "#fa3636";
+        heart.style.transform = "scale(1.1)"; // Reset size
+        heart.style.transition = "opacity 0.5s ease-in, transform 0.3s ease-in-out";
+
+        // Hide the heart after 1 second
+        setTimeout(() => {
+            heart.style.opacity = "0";
+
+        }, 1000);
+    });
+});
+}
+like();
+
+function randImg() {
+    let btn = document.querySelector("#btn")
+let elem = document.querySelector(".elem")
+let imgs = [
+    "Assets/img1.jpg",
+    "Assets/img3.jpg",
+    "Assets/img4.jpg",
+    "Assets/img5.jpg",
+    "Assets/img6.jpg",
+    "Assets/img7.jpg",
+    "Assets/img8.jpg",
+]
+let idx = 0;
+
+btn.addEventListener("click", function () {
+    if (idx >= imgs.length) {
+        idx =0;
+    }
+    let x= Math.floor(Math.random()*90) + "%";
+    let y=  Math.floor(Math.random()*90) + "%";
+    let rot= Math.random()*360 + "deg";
+    
+
+   let img = document.createElement("img")
+   img.style.position = "absolute"
+   img.style.top =  y;
+   img.style.left =  x;
+   img.style.transform =  `rotate(${rot})`;
+   let sImg = elem.appendChild(img)
+   sImg.setAttribute("src",imgs[idx])
+   idx++;
+
+})
+}
+
+randImg()
+
+
+
